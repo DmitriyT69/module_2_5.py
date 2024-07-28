2ec35704989e4daf16d958ed95cfe3476ec3d6ce
DmitriyT69 <td2598793@gmail.com>
2024-07-28 14:43:48 +0400

    Update and rename Домашняя работа по уроку "Функции в Python.Функция с параметром" to module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -1,15 +1,18 @@
 def get_matrix(n, m, value):
     matrix = []
+    row = []
     for i in range(n):
-        matrix.append(n)
+        matrix.append(value)
         for j in range(m):
-            matrix.append(value)
+            row.append(value)
+        matrix.append(row)
     print(matrix)
     return matrix
 
 result_1 = get_matrix(2,2,10)
-result_2 = get_matrix(3, 4 , 38)
-result_3 = get_matrix(4, 2, 11)
+result_2 = get_matrix(3, 5 , 42)
+result_3 = get_matrix(4, 2, 13)
+
 print(result_1)
 print(result_2)
 print(result_3)
